41241074042350bab8ea2dea56c27ecbcca4d43d
Max Suslowicz <max@suslowicz.co>
2023-09-17 10:57:00 +0100

    Lesson 9 - Pagination

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # flaskblog
+
 Flask Blog
+
+To make sure the database file is created, and to use models from this app within the REPL, you may need to run
+the following commands from within the REPL:
+
+from flaskblog import app, db
+app.app_context().push()
+db.create_all()
+
+Then the .db file is created in a folder called "Instance" in your project. 
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import secrets
 
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 
 # Pillow (image resizing)
 from PIL import Image
@@ -12,25 +12,12 @@ from flaskblog import app, db, bcrypt
 
 from flaskblog.models import AppUser, Post
 
-posts = [
-    {
-        "author": "Jane Austen",
-        "title": "Blog Post 1",
-        "content": "Hello!!",
-        "posted_date": "April 24, 2023",
-    },
-    {
-        "author": "Adolph Hitler",
-        "title": "My Struggle",
-        "content": "Fourscore and seven years ago, my name was John Galt.",
-        "posted_date": "April 20, 2023",
-    },
-]
-
 
 @app.route(rule="/")
 @app.route(rule="/home")
 def home():
+    page = request.args.get("page", default=1, type=int)
+    posts = Post.query.order_by(Post.posted_date.desc()).paginate(per_page=5, page=page)
     return render_template(template_name_or_list="home.html", posts=posts)
 
 
@@ -140,4 +127,75 @@ def account():
     image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
     return render_template(
         "account.html", title="Account", image_file=image_file, form=form
+    )
+
+
+@app.route(rule="/new-post", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            title=form.title.data, content=form.content.data, author=current_user
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash(message="Your post has been created!", category="success")
+        return redirect(url_for("home"))
+    return render_template(
+        "new_post.html", title="New Post", form=form, legend="New Post"
+    )
+
+
+@app.route(rule="/post/<int:post_id>")
+def post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+@app.route(rule="/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # No need to add to the session as the post is already in the db.
+        db.session.commit()
+        flash(message="Your post has been updated!", category="success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template(
+        "new_post.html", title="Update Post", form=form, legend="Update Post"
+    )
+
+
+@app.route(rule="/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id: int):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash(message="Your post has been deleted!", category="success")
+    return redirect(url_for("home"))
+
+
+@app.route(rule="/user/<string:username>")
+def user_posts(username):
+    user = AppUser.query.filter_by(username=username).first_or_404()
+    page = request.args.get("page", default=1, type=int)
+    posts = (
+        Post.query.filter_by(author=user)
+        .order_by(Post.posted_date.desc())
+        .paginate(per_page=5, page=page)
+    )
+    return render_template(
+        template_name_or_list="user_posts.html", posts=posts, user=user
     )
